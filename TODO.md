33f19f6abcf57c3aa9eabd66de86dd239f953997
Cloufish <cloufisz@gmail.com>
2020-10-20 14:31:42 +0200

    Changed knowledge to practical theory training

--- TODO.md
+++ TODO.md
@@ -4,7 +4,7 @@
 [] Setting up a timer with 15  minutes for each practice segment (or making it so that )
 []. Getting metronome to work constantly - except transcribing
 []. Deciding of which software/libraries to use in each segment
-  
+[] Setting Knowledge section to be more in depth
 ### TECHNIQUE - Development of right hand and left hand strength and dexterity
 
 #### 5min. Finger Gym
@@ -19,4 +19,4 @@
 
 ### Knowledge - new things: techniques, concepts, thoery, scales, arpeggios, etc.
 
-### Improvisation - add if applicable 
+### Improvisation - add if applicable
--- main.py
+++ main.py
@@ -11,19 +11,20 @@ def main():
     time_total = declaring_time_total()
     time_section = calculate_time_section(time_total)
     time_mini_section = calculate_time_mini_section(time_section)
+    time_theory_section = declaring_tile_total()/2
     metronome()
     TECHNIQUE(time_mini_section)
     TRANSCRIBING(time_section)
     REPERTOIRE(time_section)
     KNOWLEDGE(time_mini_section)
     IMPROVISATION(time_section)
-    
-    
+
+
 
 
 def calculate_time_section(time_total):
     wants_improvisation()
-    time_section = time_total 
+    time_section = time_total
     if wants_improvisation == False:
         time_section = time_total / 4  # Cause there're 4 sections without improvisation
     elif wants_improvisation == True:
@@ -46,7 +47,7 @@ def calculate_time_mini_section(time_section):
 def countdown_section(time_section):
     while time_section:
         mins, secs = divmod(time_section, 60)
-        timer = '{:02d}:{:02d}'.format(mins, secs)
+        timer = '{:02f}:{:02f}'.format(mins, secs)
         print(timer, end="\r")
         time.sleep(1)
         time_section -= 1
@@ -68,9 +69,19 @@ def countdown_mini_section(time_mini_section):
 def wants_improvisation():
     impro_yes_no = False
     impro_yes_no = input("Do you want to include improvisation in this routine? [true/False]")
-    if impro_yes_no == "True":
+    if impro_yes_no.strip().upper().startswith('T'):
         return True
-    elif impro_yes_no == "False":
+    elif impro_yes_no.strip().upper().startswith('F'):
+        return False
+    else:
+        return False
+
+def theory_level():
+    theory_level = 1
+    theory_level = 1 = int(input("""What is your level in theory knowledge from 1 to 10?(default - '1'"""))
+    if theory_level <=5:
+        return True
+    elif theory_level >=5:
         return False
     else:
         return False
@@ -79,8 +90,8 @@ def metronome():
     driver.get("https://metronom.us/en/")
     python_button = driver.find_elements_by_xpath("//*[@id='start-stop']")[0]
     python_button.click()
-    create_and_switchTab() 
-      
+    create_and_switchTab()
+
 def create_and_switchTab():
     driver.switch_to.new_window('tab')
 
@@ -91,37 +102,28 @@ def TECHNIQUE(time_mini_section):
     print("Try to 'flick' the strings with your fret fingers like you should do on hammer ons")
     print("Adnotation: https://www.justinguitar.com/guitar-lessons/finger-gym-te-110 ")
     countdown_mini_section(time_mini_section)
- 
+
     # Spider exercises
     print("Nothing to say here if you know some of spider patterns on guitar")
     time.sleep(3)
     print("If you don't here's a link - http://www.diegoruizguitar.com/tabs/8-Guitar-Spider-Warm-Up-Exercises-x-Diego-Ruiz.pdf ")
-    
+
     countdown_mini_section(time_mini_section)
     # Scale Picking
     print("It's time for scale picking!")
+    print("You must remember that there are 3 systems of playing Scales: CAGED System, 3 notes per string,  ")
     print("Choose the difficult level: ")
     scale_difficulty = input("Do you want Beginner or Intermediate scales? [B/I]")
-    if scale_difficulty == "B":
+    if scale_difficulty.strip().upper().startswith("B"):
         print("BEGINNER:")
-        create_and_switchTab()
-        driver.get("https://musiciantuts.com/learning-guitar-scales/#Minor_Pentatonic")
-        create_and_switchTab()
-        driver.get("https://musiciantuts.com/learning-guitar-scales/#Major_Pentatonic ")
-        create_and_switchTab()
-        driver.get("https://musiciantuts.com/learning-guitar-scales/#Blues_Scale ")
-    
+        print("https://musiciantuts.com/learning-guitar-scales/#Minor_Pentatonic")
+        print("https://musiciantuts.com/learning-guitar-scales/#Major_Pentatonic ")
+
         countdown_mini_section(time_mini_section)
-    if scale_difficulty =="I":
+    if scale_difficulty.strip().upper().startswith("I"):
         print("INTERMEDIATE:")
-        create_and_switchTab()
-        driver.get(" https://musiciantuts.com/learning-guitar-scales/#Major_Scale ")
-        create_and_switchTab()
-        driver.get("https://musiciantuts.com/learning-guitar-scales/#Natural_Minor_Scale")
-        create_and_switchTab()
-        driver.get("https://musiciantuts.com/learning-guitar-scales/#Harmonic_Minor ")
-        create_and_switchTab()
-        driver.get("https://musiciantuts.com/learning-guitar-scales/#Melodic_Minor ")
+        print(" https://musiciantuts.com/learning-guitar-scales/#Major_Scale ")
+        print("https://musiciantuts.com/learning-guitar-scales/#Natural_Minor_Scale")
 
         countdown_mini_section(time_mini_section)
 
@@ -130,7 +132,7 @@ def TRANSCRIBING(time_section):
     #python_button.click
     print("TRANSCRIBING section")
     print("Download songs that you want to transcribe and put them into the software called 'transcribe'!")
-    os.system('transcribe')
+    os.system('sonic-visualiser &')
     countdown_section(time_section)
 
 def  REPERTOIRE(time_section):
@@ -140,27 +142,20 @@ def  REPERTOIRE(time_section):
           "- Solo Song - Songs that would sound good without accompaniment \n"
           "- Band Songs - Songs that you should learn intensely and to learn the whole song, not only some bits! \n"
           "- Advanced Songs - When you get to advanced level, you should stop learning BBQ songs and head over into some advanced stuff!")
-
+    countdown_section(time_section)
 def KNOWLEDGE(time_mini_section):
-    print("It's time to get more knowlede :) ")
-    print("You should be spending this time on: \n"
-          "- 5 minutes - Music Theory - either the recommended one \n"
-          "- 5 minutes - Scales or chords depending on what you need to work on. \n"
-          "- 5 minutes - < something else of your choice")
-    print("While doing this, remenber to take notes")
-    time.sleep(5)
-    print("Countdown starts!")
-    time.sleep(2)
-    # Music Theory
-    print("Music theory")
-    countdown_mini_section(time_mini_section)
-    # Scales or chords
-    print("Scales or chords")
-    countdown_mini_section(time_mini_section)
-    # Something else of your choice
-    print("Something of your choice")
-    countdown_mini_section(time_mini_section)
-    
+    print("It's time to get more knowledge/theory practically : ")
+    print("This is supposed to be practical practice of music theory (sounds weird, I know!) \n"
+          "So in this section, you should always be having your guitar equipped")
+    theory_level = theory_level()
+    if theory_level == True:
+        print("""Beginners should focus on Basic Rhythm, Musical time signatures, Localising notes on the neck, Intervals between notes
+                It's in some way pointless for me to explain everything, if you don't know what these abilities are, you should google or watch videos about them on youtube! If you know what they mean, then it's still pointless for me to give further explanation REMEMBER TO MAKE IT PRACTICAL AND PLAY ON THE GUITAR""")
+        countdown_mini_section(time_section)
+    if thery_level == False:
+        print("""Intermediate players should focus on: Building Chords, Localising the Chords Notes on the neck, Finding chord grips with more creativity, Modal Harmony
+                It's in some way pointless for me to explain everything, if you don't know what these abilities are, you should google or watch videos about them on youtube! If you know what they mean, then it's still pointless for me to give further explanation. REMEMBER TO MAKE IT PRACTICAL AND PLAY ON THE GUITAR!""")
+
 def IMPROVISATION(time_section):
     if wants_improvisation == True:
         print ("It's time for the improvisation")
